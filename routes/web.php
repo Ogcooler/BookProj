5ae6467fc48d544c6b4e471691e4669eceaa2fe6
Robert <ogcooler910@gmail.com>
2020-07-06 14:53:54 +0600

    update controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/members', 'MembersController@show')->name('members11');
+
+Route::get('/list', function(){
+    return redirect()->route('members11');
+});
